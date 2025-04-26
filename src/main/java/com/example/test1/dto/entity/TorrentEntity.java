b52d475a7f8592d5c71de1b8337dbe701eb0f14c
zhengyx <1291870371@qq.com>
2025-04-26 14:29:10 +0800

    1

---
+++ src/main/java/com/example/test1/dto/entity/TorrentEntity.java
@@ -0,0 +1,186 @@
+package com.example.test1.dto.entity;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author zyx
+ * @email
+ * @date 2025-04-20
+ */
+@Data
+@TableName("t_torrent")
+public class TorrentEntity {
+
+    /**
+     *
+     */
+    @TableId
+    private Integer id;
+    /**
+     * 种子哈希
+     */
+    private byte[] infoHash;
+    /**
+     * 名称
+     */
+    private String name;
+    /**
+     * 上传文件名
+     */
+    private String filename;
+    /**
+     * 标题
+     */
+    private String title;
+    /**
+     * 简介副标题
+     */
+    private String subheading;
+    /**
+     * 封面
+     */
+    private String cover;
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 类别
+     */
+    private Integer category;
+
+    /**
+     * 状态
+     *
+     * @see Status
+     */
+    private Integer status;
+
+    /**
+     * 文件状态 0 未上传 1 已上传
+     */
+    private Integer fileStatus;
+    /**
+     * 审核人
+     */
+    private Integer reviewer;
+
+
+    /**
+     * 添加日期
+     */
+    private LocalDateTime createTime;
+
+
+    /**
+     * 修改日期
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 拥有者
+     */
+    private Integer owner;
+    /**
+     * 文件大小
+     */
+    private Long size;
+    /**
+     * 类型
+     * single(1)
+     * multi(2)
+     */
+    private Integer type;
+    /**
+     * 文件数量
+     */
+    private Integer fileCount;
+
+    /**
+     * 评论数
+     */
+    private Integer comments;
+    /**
+     * 浏览次数
+     */
+    private Integer views;
+    /**
+     * 点击次数
+     */
+    private Integer hits;
+
+    /**
+     * 可见性
+     */
+    private Integer visible;
+
+    /**
+     * 是否匿名
+     */
+    private Integer anonymous;
+
+
+    /**
+     * 下载数
+     */
+    private Integer leechers;
+    /**
+     * 做种数
+     */
+    private Integer seeders;
+
+    /**
+     * 完成次数
+     */
+    private Integer completions;
+
+    /**
+     *
+     */
+    private String remark;
+
+    /**
+     * 种子状态
+     * 0 候选中 1 已发布 2 审核不通过 3 已上架修改重审中 10 已下架
+     */
+    public interface Status {
+
+        int CANDIDATE = 0;
+
+        int PUBLISHED = 1;
+
+        int AUDIT_NOT_PASSED = 2;
+
+        int RETRIAL = 3;
+
+        int REMOVED = 10;
+
+    }
+
+    @RequiredArgsConstructor
+    public enum Type {
+        single(1),
+        multi(2);
+
+        @Getter
+        @EnumValue
+        private final int code;
+    }
+
+    public boolean isStatusOK() {
+
+
+        return status == 1;
+    }
+
+}
+
---
+++ src/main/java/com/example/test1/dto/entity/UserEntity.java
@@ -0,0 +1,221 @@
+package com.example.test1.dto.entity;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+//import com.rocketpt.server.common.exception.RocketPTException;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author zyx
+ * @email
+ * @date 2025-04-20
+ */
+@Data
+@TableName("t_user")
+public class UserEntity {
+
+
+    @TableId
+    private Integer id;
+    /**
+     * 账户名
+     */
+    @Schema(description = "username")
+    private String username;
+    /**
+     * 头像
+     */
+    @Schema(description = "头像")
+    private String avatar;
+    /**
+     * 注册时间
+     */
+    @Schema(description = "注册时间")
+    private LocalDateTime createTime;
+
+    /**
+     * 昵称
+     */
+    @Schema(description = "昵称")
+    private String nickname;
+    /**
+     * 性别 0男 1女 2其他
+     */
+    @Schema(description = "性别 0男 1女 2其他")
+    private Integer gender;
+    /**
+     * 状态 0正常 1 已锁定 2未激活
+     */
+    @Schema(description = "状态 0正常 1 已锁定 2未激活")
+    private Integer state;
+    /**
+     * 邮件地址
+     */
+    @Schema(description = "email")
+    private String email;
+
+    /**
+     * 管理备注
+     */
+    @Schema(description = "管理备注")
+    private String remark;
+    /**
+     * 上次登录时间
+     */
+    @Schema(description = "上次登录时间")
+    private LocalDateTime lastLogin;
+    /**
+     * 上次访问时间
+     */
+    @Schema(description = "上次访问时间")
+    private LocalDateTime lastAccess;
+
+    /**
+     * 上次发布offer时间
+     */
+    private LocalDateTime lastOffer;
+
+    /**
+     * 隐私级别 0 1 2
+     */
+    @Schema(description = "隐私级别")
+    private Integer privacy;
+    /**
+     * 用户等级
+     */
+    private Integer level;
+    /**
+     * 用户最大等级
+     */
+    private Integer levelMax;
+    /**
+     * 上传量
+     */
+    @Schema(description = "上传量")
+    private Long uploaded;
+    /**
+     * 下载量
+     */
+    @Schema(description = "下载量")
+    private Long downloaded;
+    /**
+     * 真实上传量
+     */
+    @Schema(description = "真实上传量")
+    private Long realUploaded;
+    /**
+     * 真实下载量
+     */
+    @Schema(description = "真实下载量")
+    private Long realDownloaded;
+    /**
+     * 做种时间
+     */
+    @Schema(description = "做种时间")
+    private Long seedtime;
+    /**
+     * 下载时间
+     */
+    @Schema(description = "下载时间")
+    private Long leechtime;
+
+    /**
+     * 警告
+     */
+    @Schema(description = "警告状态")
+    private Boolean warning;
+    /**
+     * 警告者
+     */
+    @Schema(description = "警告者")
+    private Long warningBy;
+    /**
+     * 总警告次数
+     */
+    @Schema(description = "总警告次数")
+    private Integer warningTimes;
+    /**
+     * 警告到期时间
+     */
+    @Schema(description = "警告到期时间")
+    private LocalDateTime warningUntil;
+
+    /**
+     *
+     */
+    private Integer download;
+    /**
+     *
+     */
+    private Integer upload;
+    /**
+     * 上家
+     */
+    @Schema(description = "上家ID")
+    private Integer inviter;
+    /**
+     * 魔力积分
+     */
+    @Schema(description = "魔力积分")
+    private Long bonus;
+    /**
+     * 经验值
+     */
+    @Schema(description = "经验值")
+    private Long exp;
+
+    @RequiredArgsConstructor
+    public enum Gender {
+        MALE(0),
+        FEMALE(1),
+        OTHER(2);
+
+        @EnumValue
+        @Getter
+        private final int code;
+
+        public static Gender valueof(Integer value) throws Exception {
+            if (value == null) {
+                return OTHER;
+            }
+
+            for (Gender gender : Gender.values()) {
+                if (gender.code == value) {
+                    return gender;
+                }
+            }
+
+            throw new Exception("性别错误");
+        }
+    }
+
+    @RequiredArgsConstructor
+    public enum State {
+        NORMAL(0),
+        LOCKED(1),
+        INACTIVATED(2);
+        @EnumValue
+        @Getter
+        private final int code;
+    }
+
+    public boolean userLocked() {
+        return this.state == State.LOCKED.code;
+    }
+
+    /**
+     * @return 用户状态正常
+     */
+    public boolean isUserOK() {
+
+        return this.state == State.NORMAL.code;
+    }
+
+
+}
