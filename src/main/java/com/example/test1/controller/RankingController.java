6b53da7ea3e042719f5d63afa855388b7b455112
zhengyx <1291870371@qq.com>
2025-06-07 14:47:32 +0800

    添加经验魔力值相关增减

--- src/main/java/com/example/test1/controller/RankingController.java
+++ src/main/java/com/example/test1/controller/RankingController.java
@@ -4,12 +4,13 @@ import com.example.test1.entity.Ranking;
 import com.example.test1.service.RankingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/ranking")
 public class RankingController {
